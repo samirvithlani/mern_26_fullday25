a83ff67ebcf573605a2d303d6ee987e98712181e
samirvithlani <samir.vithlani83955@gmail.com>
2026-05-01 14:21:39 +0530

    dynamic url

--- src/App.jsx
+++ src/App.jsx
@@ -12,15 +12,22 @@ import { MapDemo4 } from './components/MapDemo4'
 import {  Route, Routes } from 'react-router-dom'
 import { NetflixHome } from './components/netflix/NetflixHome'
 import { NetflixShows } from './components/netflix/NetflixShows'
+import { Navbar } from './components/Navbar'
+import { Error404 } from './components/Error404'
+import { Watch } from './components/netflix/Watch'
 
 
 function App() {
 
   return (
     <div>
+      <Navbar></Navbar>
         <Routes>
+          <Route path='/' element = {<MapDemo3/>}></Route>
           <Route path='/netflixhome' element = {<NetflixHome/>}></Route>
           <Route path='/netflixshows' element = {<NetflixShows/>}></Route>
+          <Route path='/watch/:name' element = {<Watch/>}></Route>
+          <Route path='/*' element = {<Error404/>}></Route>
         </Routes>
     </div>
   )
--- src/components/netflix/NetflixShows.jsx
+++ src/components/netflix/NetflixShows.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const NetflixShows = () => {
   return (
-    <div style={{textAlign:"center"}}>
-        <h1>NetflixShows</h1>
+    <div style={{ textAlign: "center" }}>
+      <h1>NetflixShows</h1>
+      <li>
+        <Link to="/watch/friends">Friends</Link>
+      </li>
+      <li>
+        <Link to="/watch/moneyheiest">Money Heist</Link>
+      </li>
+      <li>
+        <Link to="/watch/stranger things">Stranger Things</Link>
+      </li>
+      <li>
+        <Link to="/watch/breaking bad">Breaking Bad</Link>
+      </li>
     </div>
-  )
-}
+  );
+};
