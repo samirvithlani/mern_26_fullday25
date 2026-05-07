9194b560c2df3c857388c843fb5257b8521dd818
samirvithlani <samir.vithlani83955@gmail.com>
2026-05-07 12:11:32 +0530

    dynamic url

--- src/components/Teams.jsx
+++ src/components/Teams.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export const Teams = () => {
+    const teamList = [
+        {id:1,name:"RCB"},
+        {id:2,name:"PBKS"},
+        {id:3,name:"GT"},
+        {id:4,name:"SRH"}
+    ]
   return (
     <div style={{textAlign:"center"}}>
         <h1>TEAMS</h1>
@@ -12,6 +18,15 @@ export const Teams = () => {
             <li>
                 <Link to="/teamdetail/mi">MI</Link>
             </li>
+        </ol>
+        <ol>
+            {
+                teamList.map((t)=>{
+                    return <li>
+                        <Link to={`/teamdetail/${t.name}`}>{t.name}</Link>
+                    </li>
+                })
+            }
         </ol>
     </div>
   )
